c05cdc9326e91d1c760ddb763e5f871830561fc4
Daniel Green <77940953+impilcature@users.noreply.github.com>
2022-04-01 08:42:39 +0900

    initial refactor

--- repository/models.py
+++ repository/models.py
@@ -1,40 +1,37 @@
 from sqlalchemy import Column, String, Integer, Date
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+# Classes and models
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(50), unique=True)
 
-# Completed this model based off of Barky.  The classes from this module should be imported to ORM as string mappers
-
-
-class Role:
-    """
-      CREATE TABLE IF NOT EXISTS Role (     
-            id       INT     NOT NULL,
-            role char(50) NOT NULL,
-            PRIMARY KEY (RoleID)	
-    );
-    """
-
-    def __init__(self, id, role) -> None:
-        self.id = id
+    def __init__(self, role) -> None:
         self.role = role
 
 
-class Address:
-    """
-        CREATE TABLE IF NOT EXISTS Address (
-                AddressID    INT    NOT NULL,  
-                Street1  char(25)   NOT NULL,
-                Street2    char(25),
-                City 		CHAR(13)        NOT NULL,
-                State     char(25)   NOT NULL,
-                PostalCode char(5) NOT NULL,
-                PRIMARY KEY (AddressID)      
-        );
-    """
+class RoleSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "role")
 
-    def __init__(self, id, street1, street2, city, state, postalcode, commands) -> None:
-        self.id = id
+
+class Command(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uic = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(50))
+    region = db.Column(db.String(50))
+    street1 = db.Column(db.String(100))
+    street2 = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    postalcode = db.Column(db.String(10))
+
+    def __init__(
+        self, uic, name, region, street1, street2, city, state, postalcode
+    ):
+        self.uic = uic
+        self.name = name
+        self.region = region
         self.street1 = street1
         self.street2 = street2
         self.city = city
@@ -42,126 +39,137 @@ class Address:
         self.postalcode = postalcode
 
 
-class Command:
-    """
-        CREATE TABLE IF NOT EXISTS Command (
-                CommandID     INT    NOT NULL,
-                UIC      Char(25)  NOT NULL,
-                Name		CHAR(50)   	      NOT NULL,
-                Region		CHAR(50)   	      NOT NULL,
-                Address_AddressID INT NOT NULL,
-                Foreign key (Address_id) REFERENCES Address(AddressID),
-                PRIMARY KEY (CommandID)
-            );
-    """
-
-    def __init__(self, id, uic, name, region, addressid) -> None:
-        self.id = id
-        self.uic = uic
-        self.name = name
-        self.region = region
-        self.AddressID = addressid
+class CommandSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "uic",
+            "name",
+            "region",
+            "street1",
+            "street2",
+            "city",
+            "state",
+            "postalcode",
+        )
 
 
-class Checklist:
-    """
-        CREATE TABLE IF NOT EXISTS Checklist (
-                ChecklistID INT NOT NULL,
-                Name CHAR(25) NOT NULL
-                Reference CHAR(50) NOT NULL,
-                RevisionDate DATE NOT NULL,
-                Assessment CHAR(25),
-                Footer CHAR(25),
-                Applicability CHAR(25),
-                primary key (ChecklistID),
-    
-    """
+class Checklist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    revisiondate = db.Column(db.String(100))
+    assessment = db.Column(db.String(500))
+    footer = db.Column(db.String(500))
+    applicability = db.Column(db.String(500))
 
-    def __init__(
-        self, id, name, referenceid, revisiondate, assessment, footer, applicability
-    ) -> None:
-        self.id = id
+    def __init__(self, name, revisiondate, assessment, footer, applicability):
         self.name = name
         self.revisiondate = revisiondate
         self.assessment = assessment
         self.footer = footer
         self.applicability = applicability
-        self.referenceid = referenceid
 
 
-class Reference:
-    """
-        CREATE TABLE IF NOT EXISTS Reference (
-                ReferenceID INT NOT NULL,
-                Title TEXT NOT NULL,
-                Page TEXT,
-                Paragragh TEXT,
-                PRIMARY KEY (ReferenceID)
-            );  
-    """
+class ChecklistSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "revisiondate", "assessment", "footer", "applicability")
 
-    def __init__(self, id, title, page, paragraph) -> None:
-        self.id = id
+
+class Reference(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    page = db.Column(db.String(10))
+    paragraph = db.Column(db.String(500))
+
+    def __init__(self, title, page, paragraph):
         self.title = title
         self.page = page
         self.paragraph = paragraph
 
 
-class Subsection:
-    """	
-        CREATE TABLE IF NOT EXISTS Subsection (
-                SubsectionID INT NOT NULL,
-                Title Char(25) NOT NULL,
-                PRIMARY KEY (SubsectionID)
-        );
-    """
+class ReferenceSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "page", "paragraph")
 
-    def __init__(self, id, title) -> None:
-        self.id = id
+
+class Subsection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+
+    def __init__(self, title):
         self.title = title
 
 
-class Person:
-    """	
-CREATE TABLE IF NOT EXISTS Person (
-		PersonID     INT       NOT NULL,
-		LastName      CHAR(25)   NOT NULL,
-		FirstName CHAR(25) NOT NULL,
-		Phone Char(25) NOT NULL,
-		Command_CommandID INT NOT NULL,
-		Role_RoleID INT NOT NULL,
-		Address_AddressID INT NOT NULL,
-		PRIMARY KEY (PersonID),
-		Foreign key (CommandID) REFERENCES Command(CommandID),
-		Foreign key (Role_ID) REFERENCES Role(RoleID),
-		Foreign key (Address_id) REFERENCES Address(AddressID)
-  );	
+class SubsectionSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title")
 
-"""
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lastname = db.Column(db.String(50))
+    firstname = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
+    street1 = db.Column(db.String(100))
+    street2 = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    postalcode = db.Column(db.String(10))
 
     def __init__(
-        self, id, lastname, firstname, phone, commandid, roleid, addressid
-    ) -> None:
-        self.id = id
+        self,
+        lastname,
+        firstname,
+        phone,
+        addressid,
+        street1,
+        street2,
+        city,
+        state,
+        postalcode,
+    ):
         self.lastname = lastname
         self.firstname = firstname
         self.phone = phone
-        self.commandid = commandid
-        self.roleid = roleid
         self.addressid = addressid
+        self.street1 = street1
+        self.street2 = street2
+        self.city = city
+        self.state = state
+        self.postalcode = postalcode
 
 
-class Question:
-    """		QuestionID INT NOT NULL,
-        Verbiage TEXT NOT NULL,
-		Results TEXT,
-		Reference_ReferenceID INT,
-		Foreign key (Reference_ReferenceID) REFERENCES Reference(ReferenceID),
-        PRIMARY KEY (QuestionID);
-        """
+class PersonSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "lastname",
+            "firstname",
+            "phone",
+            "addressid",
+            "street1",
+            "street2",
+            "city",
+            "state",
+            "postalcode",
+        )
 
-    def __init__(self, id, question, results, referenceID) -> None:
-        self.id - id
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500))
+    results = db.Column(db.String(500))
+
+    def __init__(self, question, results, referenceID):
         self.question = question
         self.results = results
-        self.referenceID = referenceID
+
+
+class QuestionSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "question",
+            "results",
+        )
+
--- services/database.py
+++ services/database.py
@@ -4,117 +4,18 @@ This may not be necessary with SQLalchemy  Look at inner workings to be sure.
 
 """
 
-"""
-This module supports the following schema:
-
-CREATE TABLE IF NOT EXISTS Role (     
-        RoleID       INT     NOT NULL,
-        Role char(50) NOT NULL,
-        PRIMARY KEY (RoleID)	
-);
-
- CREATE TABLE IF NOT EXISTS Address (
-         AddressID    INT    NOT NULL,  
-         Street1  char(25)   NOT NULL,
-         Street2    char(25),
-         City 		CHAR(13)        NOT NULL,
-         State     char(25)   NOT NULL,
-         PostalCode char(5) NOT NULL,
-         PRIMARY KEY (AddressID)      
-  );
-
-CREATE TABLE IF NOT EXISTS Command (
-		CommandID     INT    NOT NULL,
-		UIC      Char(25)  NOT NULL,
-		Name		CHAR(50)   	      NOT NULL,
-		Region		CHAR(50)   	      NOT NULL,
-		Address_AddressID INT NOT NULL,
-		Foreign key (Address_id) REFERENCES Address(AddressID),
-		PRIMARY KEY (CommandID)
-  );
-  
-  CREATE TABLE IF NOT EXISTS Checklist (
-		ChecklistID INT NOT NULL,
-		Name CHAR(25) NOT NULL
-        Reference CHAR(50) NOT NULL,
-		RevisionDate DATE NOT NULL,
-		Assessment CHAR(25),
-		Footer CHAR(25),
-		Applicability CHAR(25),
-        primary key (ChecklistID),
-);
-
-CREATE TABLE IF NOT EXISTS Reference (
-		ReferenceID INT NOT NULL,
-        Title TEXT NOT NULL,
-		Page TEXT,
-		Paragragh TEXT,
-        PRIMARY KEY (ReferenceID)
-	);  
-	
-
-CREATE TABLE IF NOT EXISTS Checklist (
-		ChecklistID INT NOT NULL,
-		Name CHAR(25) NOT NULL
-        Reference CHAR(50) NOT NULL,
-		RevisionDate DATE NOT NULL,
-		Assessment CHAR(25),
-		Footer CHAR(25),
-		Applicability CHAR(25),
-        primary key (ChecklistID),
-);
-
-CREATE TABLE IF NOT EXISTS Subsection ( -- Left off here
-		SubsectionID INT NOT NULL,
-		Title Char(25) NOT NULL,
-		PRIMARY KEY (SubsectionID)
-  );
-
-CREATE TABLE IF NOT EXISTS Payment (
-		PaymentID INT NOT NULL,
-		Invoice_InvoiceID INT NOT NULL,
-        Employee_EmployeeID INT NOT NULL,
-		PaymentDate date NOT NULL,
-        PaymentMethod char(25) NOT NULL,
-        PaymentAmount FLOAT NOT NULL,
-		PRIMARY KEY (PaymentID),
-		FOREIGN KEY (Invoice_InvoiceID) REFERENCES Invoice(InvoiceID),
-        FOREIGN KEY (Employee_EmployeeID) REFERENCES Employee(EmployeeID)
-	);
-
-CREATE TABLE IF NOT EXISTS Question (
-		QuestionID INT NOT NULL,
-        Verbiage TEXT NOT NULL,
-		Results TEXT,
-		Reference_ReferenceID INT,
-		Foreign key (Reference_ReferenceID) REFERENCES Reference(ReferenceID),
-        PRIMARY KEY (QuestionID);
-	);
-	
-CREATE TABLE IF NOT EXISTS Person (
-		PersonID     INT       NOT NULL,
-		LastName      CHAR(25)   NOT NULL,
-		FirstName CHAR(25) NOT NULL,
-		Phone Char(25) NOT NULL,
-		Command_CommandID INT NOT NULL,
-		Role_RoleID INT NOT NULL,
-		Address_AddressID INT NOT NULL,
-		PRIMARY KEY (PersonID),
-		Foreign key (CommandID) REFERENCES Command(CommandID),
-		Foreign key (Role_ID) REFERENCES Role(RoleID),
-		Foreign key (Address_id) REFERENCES Address(AddressID)
-  );	
-
-"""
 
 import sqlite3
+import os
+from ..api.flaskapi import app, basedir
+from flask_sqlalchemy import SQLAlchemy
 
 
-class DatabaseManager:
-    def __init__(self, database_filename) -> None:
-        # added this to persist the name of the database file
-        self.database_filename = database_filename
-        self.connection = sqlite3.connect(database_filename)
+# Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "cgi-checklist.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    def __del__(self):
-        self.connection.close()
+# Init db
+db = SQLAlchemy(app)
